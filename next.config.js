2b0befeac6ab4090d7fd1a126d928ca2296e89b4
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2023-10-31 21:26:59 +0100

    Dockerize application

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'drive.google.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
       },
+    ],
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
