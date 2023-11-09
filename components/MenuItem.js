4a53a4312128addab457b21f38f83f83375092ad
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2023-11-09 14:02:27 +0100

    Refactor navigation and style states

--- components/MenuItem.js
+++
@@ -1,8 +0,0 @@
-import Link from "next/link"
-import styles from './menuitem.module.css';
-
-export default function MenuItem({ text, href }) {
-    return (
-        <Link className={`${styles["menu-item"]}`} href={href}> {text} </Link>
-    );
-}
--- components/SideBar.js
+++ components/SideBar.js
@@ -1,17 +1,19 @@
+"use client"
 import Link from "next/link";
-import MenuItem from './MenuItem.js';
+import { usePathname } from "next/navigation";
 import styles from './sidebar.module.css';
 
 export default function SideBar() {
+    const pathname = usePathname();
     return (
         <div className={`${styles["sidebar"]}`}>
             <Link href="/">
                 <img src="/title-logo.svg" alt="" className={styles.logo} />
             </Link>
-            <MenuItem href="/projects" text="Projects" />
-            <MenuItem href="/my-toolbox" text="My toolbox" />
-            {/* <MenuItem href="/blog" text="Blog" /> */}
-            <MenuItem href="/about" text="About me" />
+
+            <Link className={`${styles["menu-item"]} ${pathname == "/projects" ? styles["selected"] : ""}`} href="/projects"> Projects </Link>
+            <Link className={`${styles["menu-item"]} ${pathname == "/about" ? styles["selected"] : ""}`} href="/about"> About me </Link>
+
             <div className={`${styles["social-link-group"]}`}>
                 <Link href="https://github.com/elincarls" target="_blank">
                     <img src="/github.svg" alt="" className={styles.socialLogo} />
