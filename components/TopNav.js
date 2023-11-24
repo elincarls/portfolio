e4411630b6048fea97190033f585c941cf8cbd42
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2023-11-24 12:41:02 +0100

    Make nav drawer close on click outside and when selecting a link to navigate to

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,25 +1,51 @@
-"use client"
+"use client";
 import Link from "next/link";
-import { useState } from "react";
-import { usePathname } from "next/navigation";
-import styles from './topNav.module.css'
+import { useState, useRef, useEffect} from "react";
+import { usePathname, useRouter } from "next/navigation";
+import styles from "./topNav.module.css";
 
 export default function TopNav() {
-    // const pathname = usePathname();
-    const [display, setDisplay] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const ref = useRef(); 
 
-    return (
-        <>
-            <div className={`${styles["navbar"]}`}>
-            <Link href="/">
-                <img src="/title-logo-mobile.svg" alt="" className={styles.logo} />
-            </Link>
-                <button className={`${styles["menu-button"]}`} onClick={() => setDisplay((prevDisplay) => !prevDisplay)}> Menu</button>
-            </div>
-            <div className={`${styles["menu"]}`} style={{ display: display ? "flex" : "none" }}>
-                <Link href="/projects"> Projects </Link>
-                <Link href="/about"> About me </Link>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    const handler = (event) => {
+      if (
+        drawerOpen &&
+        ref.current &&
+        !ref.current.contains(event.target)
+      ) {
+        setDrawerOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [drawerOpen]);
+
+  return (
+    <>
+      <div className={`${styles["navbar"]}`}>
+        <Link href="/">
+          <img src="/title-logo-mobile.svg" alt="" className={styles.logo} />
+        </Link>
+        <button
+          className={`${styles["menu-button"]}`}
+          onClick={() => setDrawerOpen((prev) => !prev)}
+        >
+          {" "}
+          Menu
+        </button>
+      </div>
+      <div
+        className={`${styles["menu"]}`}
+        style={{ display: drawerOpen ? "flex" : "none" }}
+        ref={ref}
+      >
+        <Link href="/projects" onClick={() => setDrawerOpen((prev) => !prev)}> Projects </Link>
+        <Link href="/about" onClick={() => setDrawerOpen((prev) => !prev)}> About me </Link>
+      </div>
+    </>
+  ); 
 }
