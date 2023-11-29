a439fb9dcc2df56a112ace879c1d5b3074182157
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2023-11-29 14:21:34 +0100

    Remove api

--- app/api/projects/route.js
+++
@@ -1,9 +0,0 @@
-import connectMongoDB from "../../../lib/mongodb"
-import Project from "../../../models/project"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-    await connectMongoDB();
-    const projects = await Project.find();
-    return NextResponse.json({ projects });
-}
