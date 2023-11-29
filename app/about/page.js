12fc0116cf10e6746c4f1f262ee3265f4e557a89
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2023-11-29 10:35:38 +0100

    Add 'Download CV' button and some placeholder content to 'About me' page

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,23 @@
-import ContentHeader from "../../components/ContentHeader"
+import ContentHeader from "../../components/ContentHeader";
+import styles from "./../page.module.css";
+import "../globals.css";
 
-  export default function AboutMe() {
-    return (
-      <ContentHeader text="About me"/>
-    ) 
-  }
+export default function AboutMe() {
+  return (
+    <div className={`${styles["about-me"]}`}>
+      <ContentHeader text="About me" />
+      <div className={`${["paragraphs"]}`}>
+        <p>
+        Lorem ipsum dolor sit amet consectetur. Eu urna tellus volutpat justo vel neque molestie euismod.
+        </p>
+      </div>
+      <a
+        className={`${styles["filled-btn"]}`}
+        href="/elin_carlsson_CV_en_20231127.pdf"
+        target="_blank"
+      >
+        Download CV
+      </a>
+    </div>
+  );
+}
