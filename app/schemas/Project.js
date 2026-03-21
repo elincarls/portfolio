3a33e4539c9a06dc49b5363428db0372b44f630f
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2026-03-21 19:06:41 +0100

    Update Project schema and add content management system

--- app/schemas/Project.js
+++ app/schemas/Project.js
@@ -1,4 +1,42 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
+const sectionSchema = new mongoose.Schema(
+  { sectionType: { type: String, required: true } },
+  { discriminatorKey: "sectionType", _id: false },
+);
+
+const paragraphSchema = new mongoose.Schema({
+  header: {
+    type: String,
+    required: false,
+  },
+  body: {
+    type: [String],
+    required: true,
+  },
+});
+
+const imageSchema = new mongoose.Schema({
+  link: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: false,
+  },
+  caption: {
+    type: String,
+    required: false,
+  },
+});
+
+const bulletListSchema = new mongoose.Schema({
+  body: {
+    type: [String],
+    required: true,
+  },
+});
 
 const projectSchema = new mongoose.Schema({
   title: {
@@ -15,8 +53,18 @@ const projectSchema = new mongoose.Schema({
   },
   tags: {
     type: [String],
-    required: false
-  }
-})
+    required: false,
+  },
+  tldr: {
+    type: String,
+    required: false,
+  },
+  sections: [sectionSchema],
+});
 
-export default mongoose.models.Project || mongoose.model('Project', projectSchema)
+projectSchema.path("sections").discriminator("paragraph", paragraphSchema);
+projectSchema.path("sections").discriminator("image", imageSchema);
+projectSchema.path("sections").discriminator("bullet-list", bulletListSchema);
+
+export default mongoose.models.Project ||
+  mongoose.model("Project", projectSchema);
