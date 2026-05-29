f0d4ab4b3d71af561eecf90ca86e1185f8d1ef40
Elin Carlsson <39401861+elincarls@users.noreply.github.com>
2026-05-29 23:33:30 +0200

    Fix correct weights for Schibsted Grotesk font

--- app/fonts.js
+++ app/fonts.js
@@ -1,6 +1,7 @@
 import { Schibsted_Grotesk, Lora } from "next/font/google";
 
 export const schibsted_grotesk = Schibsted_Grotesk({
+  weight: ["400", "500", "600"],
   subsets: ["latin"],
   display: "swap",
   variable: "--font-schibsted-grotesk",
